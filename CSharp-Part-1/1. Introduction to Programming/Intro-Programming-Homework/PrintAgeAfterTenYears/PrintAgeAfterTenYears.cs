f0222fbeb65d81825a4be5bc90d4577b650b37d9
r1f <ivaylo.arnaudov12@gmail.com>
2015-01-10 17:19:56 +0200

    Added the text files.

--- CSharp-Part-1/1. Introduction to Programming/Intro-Programming-Homework/PrintAgeAfterTenYears/PrintAgeAfterTenYears.cs
+++ CSharp-Part-1/1. Introduction to Programming/Intro-Programming-Homework/PrintAgeAfterTenYears/PrintAgeAfterTenYears.cs
@@ -9,10 +9,32 @@ class PrintAgeAfterTenYears
         Console.WriteLine("Enter birthday (DD/MM/YYYY): ");
         string[] inputStr = Console.ReadLine().Split('/');
 
+        // It's a good idea to catch exceptions thrown from Split here in case of wrong format, but since we haven't talked about exceptions yet it's better to keep it as simple as possible.
+
+        int inputDay = int.Parse(inputStr[0]);
+        int inputMonth = int.Parse(inputStr[1]);
         int inputYear = int.Parse(inputStr[2]);
         DateTime today = DateTime.Today;
-        int currentAge = today.Year - inputYear;
-        int newAge = currentAge + 10;
-        Console.WriteLine("I am now {0} years old and I will be {1} years old in 10 years. ", currentAge, newAge);
+
+        if (inputYear > today.Year)
+        {
+            Console.WriteLine("Invalid birthday date.");
+        }
+        else
+        {
+            int currentAge = today.Year - inputYear;
+            if (inputMonth > today.Month)
+            {
+                currentAge--;
+            }
+            else
+            {
+                if (inputDay > today.Day)
+                {
+                    currentAge--;
+                }
+            }
+            Console.WriteLine("I am currently {0} years old and I will be {1} years old at this date after 10 years.", currentAge, currentAge + 10);
+        }
     }
 }
