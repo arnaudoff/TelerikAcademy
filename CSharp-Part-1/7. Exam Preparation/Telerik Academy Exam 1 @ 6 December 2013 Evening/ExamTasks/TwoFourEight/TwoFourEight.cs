d4b364685a3cdae850f4f52a7a9d2b94493c332d
r1f <ivaylo.arnaudov12@gmail.com>
2015-01-12 01:12:50 +0200

    Added some tasks

---
+++ CSharp-Part-1/7. Exam Preparation/Telerik Academy Exam 1 @ 6 December 2013 Evening/ExamTasks/TwoFourEight/TwoFourEight.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class TwoFourEight
+{
+    static void Main()
+    {
+        int firstNumber = int.Parse(Console.ReadLine());
+        int secondNumber = int.Parse(Console.ReadLine());
+        int thirdNumber = int.Parse(Console.ReadLine());
+
+        ulong result = 0;
+
+        switch(secondNumber) 
+        {
+            case 2:
+                result = (ulong)firstNumber % (ulong)thirdNumber;
+                break;
+            case 4:
+                result = (ulong)firstNumber + (ulong)thirdNumber;
+                break;
+            case 8:
+                result = (ulong)firstNumber * (ulong)thirdNumber;
+                break;
+        }
+
+        if (result % 4 == 0)
+        {
+            ulong nonReminderResult = result / 4;
+            Console.WriteLine(nonReminderResult);
+        }
+        else
+        {
+            ulong remainerResult = result % 4;
+            Console.WriteLine(remainerResult);
+        }
+
+        Console.WriteLine(result);
+    }
+}
