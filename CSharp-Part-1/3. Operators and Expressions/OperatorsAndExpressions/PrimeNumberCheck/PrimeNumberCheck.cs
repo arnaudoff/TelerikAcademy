fcb3fb5681433f772176f3f74db295489821f656
r1f <ivaylo.arnaudov12@gmail.com>
2015-01-16 01:21:25 +0200

    Fixed task

---
+++ CSharp-Part-1/3. Operators and Expressions/OperatorsAndExpressions/PrimeNumberCheck/PrimeNumberCheck.cs
@@ -0,0 +1,25 @@
+﻿// Write an expression that checks if given positive integer number n (n = 100) is prime (i.e. it is divisible without remainder only to itself and 1).
+
+using System;
+
+class PrimeNumberCheck
+{
+    static void Main()
+    {
+        int inputNumber = int.Parse(Console.ReadLine());
+
+        bool isPrime = true;
+        if (inputNumber <= 1)
+        {
+            isPrime = false;
+        }
+        for (int i = 2; i <= Math.Sqrt(inputNumber); i++)
+        {
+            if (inputNumber % i == 0)
+            {
+                isPrime = false;
+            }
+        }
+        Console.WriteLine(isPrime);
+    }
+}
