c1ef0680d6fceb7a7f6cd68eeff95c0aa50dd432
r1f <ivaylo.arnaudov12@gmail.com>
2015-01-17 22:43:32 +0200

    Added operators & expressions homework

---
+++ CSharp-Part-1/3. Operators and Expressions/OperatorsAndExpressions/BitExchangeAdvanced/BitExchangeAdvanced.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Write a program that exchanges bits {p, p+1, …, p+k-1} with bits {q, q+1, …, q+k-1} of a given 32-bit unsigned integer.
+ * The first and the second sequence of bits may not overlap.
+ */
+
+using System;
+
+class BitExchangeAdvanced
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number: ");
+        long inputNumber = long.Parse(Console.ReadLine());
+        Console.WriteLine("Enter first position (p): ");
+        int firstSequencePosition = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter second position (q): ");
+        int secondSequencePosition = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter sequence length (k): ");
+        int sequenceLength = int.Parse(Console.ReadLine());
+
+        if (firstSequencePosition + sequenceLength - 1 > 31 || secondSequencePosition + sequenceLength - 1 > 31)
+        {
+            Console.WriteLine("The resultant sequence can not exceed a 32-bit unsigned integer");
+        }
+        else if (secondSequencePosition < firstSequencePosition + sequenceLength - 1 && firstSequencePosition < secondSequencePosition + 1)
+        {
+            Console.WriteLine("The sequences can not overlap.");
+        }
+        else
+        {
+            long firstSequence = (inputNumber >> firstSequencePosition) & ((1L << sequenceLength) - 1);
+            long secondSequence =  (inputNumber >> secondSequencePosition) & ((1L << sequenceLength) - 1);
+            long xoredSequences = (firstSequence ^ secondSequence);
+            xoredSequences = (xoredSequences << firstSequencePosition) | (xoredSequences << secondSequencePosition);
+            long result = inputNumber ^ xoredSequences;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ CSharp-Part-1/3. Operators and Expressions/OperatorsAndExpressions/BitsExchange/BitsExchange.cs
@@ -0,0 +1,25 @@
+﻿// Write a program that exchanges bits 3, 4 and 5 with bits 24, 25 and 26 of given 32-bit unsigned integer.
+
+using System;
+
+class BitsExchange
+{
+    static void Main()
+    {
+        int inputNumber = int.Parse(Console.ReadLine());
+
+        /*
+         * Since the distance is hardcoded, we are going to store the bits in the interval 3-5 (the least significant bit side) and 24-26 (the most significant bit side) 
+         * by using hexadecimal values that represent the binary masks instead of shifting. Then, another mask is created that clears 24-26
+         * and 3-5. Finally, shift the already extracted sides (lsbSide and msbSide) by 21 positions respectively left and right and exchange the bits by OR-ing with the masks.
+         */
+
+        int lsbSide = inputNumber & 0x00000038;
+        int msbSide = inputNumber & 0x07000000;
+        inputNumber &= ~0x07000038;
+        inputNumber |= lsbSide << 21;
+        inputNumber |= msbSide >> 21;
+        Console.WriteLine(inputNumber);
+
+    }
+}
