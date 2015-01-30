33e4c836dae95add5307ba05c610d32ee5dd7de6
r1f <ivaylo.arnaudov12@gmail.com>
2015-01-30 17:28:02 +0200

    Refactoring of Enigmanation

--- CSharp-Part-1/7. Exam Preparation/Telerik Academy Exam 1 @ 6 December 2013 Evening/ExamTasks/GameOfPage/GameOfPage.cs
+++ CSharp-Part-1/7. Exam Preparation/Telerik Academy Exam 1 @ 6 December 2013 Evening/ExamTasks/GameOfPage/GameOfPage.cs
@@ -13,7 +13,7 @@ namespace GameOfPage
                 inputArray[i] = Console.ReadLine();
             }
             string inputStr;
-            while (parseCommand(inputArray, inputStr = Console.ReadLine()));
+            while (parseCommand(inputArray, inputStr = Console.ReadLine())) ;
         }
 
         /*
@@ -21,11 +21,81 @@ namespace GameOfPage
          * Complete cookie -> 1
          * Broken cookie -> 2
          * Cookie crumb -> 3
-         * Nothing -> -1
+         * Nothing -> 0
          */
+    
         private static int checkCookie(string[] inputArray, int row, int col)
         {
-            // TODO: implement checkCookie
+            bool isBroken = false;
+            bool isCookie = true;
+            if (col == 0 && row == 0)
+            {
+                if (inputArray[0][1] == 1 || inputArray[1][1] == 1 || inputArray[1][0] == 1)
+                {
+                    isBroken = true;
+                }
+            }
+            else if (col == 0 && row != 0)
+            {
+                if (inputArray[row - 1][0] == 1 || inputArray[row + 1][0] == 1 || inputArray[row - 1][1] == 1 || inputArray[row + 1][1] == 1 || inputArray[row][1] == 1)
+                {
+                    isBroken = true;
+                }
+            }
+            else if (col != 0 && row == 0)
+            {
+                if (inputArray[0][col + 1] == 1 || inputArray[0][col - 1] == 1 || inputArray[1][col + 1] == 1 || inputArray[1][col - 1] == 1 || inputArray[1][col] == 1)
+                {
+                    isBroken = true;
+                }
+            }
+            else if (col == 15 && row == 15)
+            {
+                if (inputArray[15][14] == 1 || inputArray[14][14] == 1 || inputArray[14][15] == 1)
+                {
+                    isBroken = true;
+                }
+            }
+            else if (col == 15 && row != 15)
+            {
+                if (inputArray[row][14] == 1 || inputArray[row - 1][14] == 1 || inputArray[row + 1][14] == 1 || inputArray[row - 1][15] == 1 || inputArray[row + 1][15] == 1)
+                {
+                    isBroken = true;
+                }
+            }
+            else if (col != 15 && row == 15)
+            {
+                if (inputArray[15][col - 1] == 1 || inputArray[15][col + 1] == 1 || inputArray[14][col] == 1 || inputArray[14][col - 1] == 1 || inputArray[14][col + 1] == 1)
+                {
+                    isBroken = true;
+                }
+            }
+            else
+            {
+                // Check with ifs... Again.
+            }
+            if (isBroken == false && isCookie == false)
+            {
+                // Cookie crumb.
+                if (inputArray[row][col] == 1)
+                {
+                    return 3;
+                }
+                // Nothing
+                else
+                {
+                    return 0;
+                }
+            }
+            if (isBroken == true)
+            {
+                return 2;
+            }
+            if (isCookie)
+            {
+                return 1;
+            }
+            return -1;
         }
         private static bool parseCommand(string[] inputArray, string inputStr)
         {
