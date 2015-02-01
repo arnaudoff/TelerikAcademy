f37284f014b662192d3d70ba0ef59a83d69b8aed
r1f <ivaylo.arnaudov12@gmail.com>
2015-02-01 19:56:09 +0200

    Added some exam prep

---
+++ CSharp-Part-1/7. Exam Preparation/Telerik Academy Exam 1 @ 27 Dec 2012/ExamTasks/NextDate/NextDate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class NextDate
+{
+    static void Main()
+    {
+        int day = int.Parse(Console.ReadLine());
+        int month = int.Parse(Console.ReadLine());
+        int year = int.Parse(Console.ReadLine());
+
+        DateTime today = new DateTime(year, month, day);
+        DateTime tomorrow = today.AddDays(1);
+        Console.WriteLine("{0}.{1}.{2}", tomorrow.Day, tomorrow.Month, tomorrow.Year);
+    }
+}
---
+++ CSharp-Part-1/7. Exam Preparation/Telerik Academy Exam 1 @ 27 Dec 2012/ExamTasks/TribonacciTriangle/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        long[] numbers = new long[3];
+        numbers[0] = long.Parse(Console.ReadLine());
+        numbers[1] = long.Parse(Console.ReadLine());
+        numbers[2] = long.Parse(Console.ReadLine());
+
+        byte l = byte.Parse(Console.ReadLine());
+
+        int n = 0;
+        // Calculate N
+        for (int i = 0; i < l; i++)
+        {
+            n = n + l - i;
+        }
+
+        long nextnumber = 0L;
+        Console.WriteLine(numbers[0]);
+        Console.WriteLine("{0} {1}", numbers[1], numbers[2]);
+        int newline_number = 3;
+        for (int i = 1; i <= n - 3; i++)
+        {
+            nextnumber = numbers[0] + numbers[1] + numbers[2];
+            if (i != newline_number)
+            {
+                Console.Write("{0} ", nextnumber);
+            }
+            else
+            {
+                Console.Write("{0}", nextnumber);
+            }
+            numbers[0] = numbers[1];
+            numbers[1] = numbers[2];
+            numbers[2] = nextnumber;
+            if (i == newline_number)
+            {
+                Console.WriteLine();
+                newline_number = 2 * newline_number + 1;
+            }
+        }
+
+    }
+}
