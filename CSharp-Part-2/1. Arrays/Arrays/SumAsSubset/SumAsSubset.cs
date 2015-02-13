9355ffc2eb9c5a325ee885bfecdd1761e82bf8d0
r1f <ivaylo.arnaudov12@gmail.com>
2015-02-14 00:54:16 +0200

    First homework done

--- CSharp-Part-2/1. Arrays/Arrays/SumAsSubset/SumAsSubset.cs
+++ CSharp-Part-2/1. Arrays/Arrays/SumAsSubset/SumAsSubset.cs
@@ -14,16 +14,54 @@ class SumAsSubset
         int n = int.Parse(Console.ReadLine());
 
         Console.WriteLine("Enter array contents: ");
-        int[] numbers = new int[n];
+        int[] inputArray = new int[n];
 
         for (int i = 0; i < n; i++)
         {
-            numbers[i] = int.Parse(Console.ReadLine());
+            inputArray[i] = int.Parse(Console.ReadLine());
         }
 
         Console.WriteLine("Enter sum to search for: ");
         int sum = int.Parse(Console.ReadLine());
 
-        
+        int result = GetSum(inputArray, n, sum);
+        if (result == 1)
+        {
+            Console.WriteLine("The sum was found in a subset.");
+        }
+        else
+        {
+            Console.WriteLine("The sum was not found in any subset.");
+        }
+
+    }
+
+    // A naive solution for this would be O(n^2). The complexity of this algorithm, however, is O(n).
+
+    static int GetSum(int[] inputArray, int n, int sum)
+    {
+        int currentSum = inputArray[0];
+        int start = 0;
+
+        for (int i = 1; i < n; i++)
+        {
+            // Remove the first element if we've exceeded the sum. It improves the time complexity to linear.
+            while (currentSum > sum && start < i - 1)
+            {
+                currentSum = currentSum - inputArray[start];
+                start++;
+            }
+
+            if (currentSum == sum)
+            {
+                return 1;
+            }
+
+            if (i < n)
+            {
+                currentSum += inputArray[i];
+            }
+        }
+        return -1;
     }
 }
--- CSharp-Part-2/1. Arrays/Arrays/SumAsSubsetK/SumAsSubsetK.cs
+++ CSharp-Part-2/1. Arrays/Arrays/SumAsSubsetK/SumAsSubsetK.cs
@@ -9,5 +9,61 @@ class SumAsSubsetK
 {
     static void Main()
     {
+        Console.WriteLine("Enter array length: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter array contents: ");
+        int[] inputArray = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            inputArray[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Enter sum to search for: ");
+        int sum = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter amount of elements: ");
+        int k = int.Parse(Console.ReadLine());
+
+        int result = GetSum(inputArray, n, sum, k);
+        if (result == 1)
+        {
+            Console.WriteLine("The sum {0} was found in a subset in the given range k (k = {1}).", sum, k);
+        }
+        else
+        {
+            Console.WriteLine("The sum {0} was not found or was not present in the given range k (k = {1}).", sum, k);
+        }
+
+    }
+
+    static int GetSum(int[] inputArray, int n, int sum, int k)
+    {
+        int currentSum = inputArray[0];
+        int start = 0;
+
+        for (int i = 1; i < n; i++)
+        {
+            while (currentSum > sum && start < i - 1)
+            {
+                currentSum = currentSum - inputArray[start];
+                start++;
+            }
+
+            if (currentSum == sum)
+            {
+                if (i - start == k)
+                {
+                    return 1;
+                }
+            }
+
+            if (i < n)
+            {
+                currentSum += inputArray[i];
+            }
+        }
+        return -1;
     }
 }
