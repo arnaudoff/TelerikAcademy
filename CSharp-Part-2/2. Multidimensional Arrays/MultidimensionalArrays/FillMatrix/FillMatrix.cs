a88ce38f7832b4faac75eb6066a19eab66d6a44f
r1f <ivaylo.arnaudov12@gmail.com>
2015-02-14 01:10:56 +0200

    Added tasks

---
+++ CSharp-Part-2/2. Multidimensional Arrays/MultidimensionalArrays/FillMatrix/FillMatrix.cs
@@ -0,0 +1,11 @@
+﻿// Write a program that fills and prints a matrix of size (n, n) as shown below:
+
+using System;
+
+class FillMatrix
+{
+    static void Main()
+    {
+
+    }
+}
---
+++ CSharp-Part-2/2. Multidimensional Arrays/MultidimensionalArrays/MaximalSum/MaximalSum.cs
@@ -0,0 +1,11 @@
+﻿// Write a program that reads a rectangular matrix of size N x M and finds in it the square 3 x 3 that has maximal sum of its elements.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+
+    }
+}
