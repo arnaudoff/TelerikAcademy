0f4b90f4bc2264875b981b2b3ead259ba5c9fe29
r1f <ivaylo.arnaudov12@gmail.com>
2015-03-13 23:07:28 +0200

    Finished generics

--- OOP/2. Defining classes - Part 2/DefiningClassesPartTwo/GenericList/GenericList.cs
+++ OOP/2. Defining classes - Part 2/DefiningClassesPartTwo/GenericList/GenericList.cs
@@ -1,8 +1,10 @@
 ﻿namespace GenericList
 {
     using System;
+    using System.Collections.Generic;
+    using System.Text;
 
-    public class GenericList<T>
+    public class GenericList<T> where T : IComparable
     {
 	    private const int INITIAL_SIZE = 8;
         private T[] values;
@@ -14,7 +16,7 @@
             this.Count = 0;
         }
 
-        public int Count {get; set; }
+        public int Count {get; private set; }
 
         public int Capacity {get; set; }
 
@@ -22,7 +24,7 @@
         {
             get
             {
-                if (index >= 0 && index < this.values.Length)
+                if (index >= 0 && index < this.Count)
                 {
                     return this.values[index];
                 }
@@ -30,19 +32,151 @@
                 {
                     throw new IndexOutOfRangeException("Index was outside the bounds of the list.");
                 }
-
             }
             set
             {
-                if (index >= 0 && index < this.values.Length)
+                if (index >= 0 && index < this.Count)
                 {
                     this.values[index] = value;
                 }
                 else
                 {
-                    throw new IndexOutOfRangeException("Index was outside the bounds of the list");
+                    throw new IndexOutOfRangeException("Index was outside the bounds of the list.");
                 }
             }
+        }
+
+        public void Add(T element)
+        {
+            if (this.Count >= values.Length)
+            {
+                this.Resize();
+            }
+
+            this.values[this.Count] = element;
+            this.Count++;
+        }
+
+        // Returns -1 if the element is not present in the array.
+        public int IndexOf(T element)
+        {
+            int index = -1;
+
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (this.values[i].Equals(element))
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            return index;
+        }
+        public void InsertAt(int index, T element)
+        {
+            if (index >= 0 && index < this.Count)
+            {
+                if (index + 1 == this.values.Length)
+                {
+                    Resize();
+                }
+
+                for (int i = this.Count; i > index; i--)
+                {
+                    this.values[i] = this.values[i - 1];
+                }
+
+                this.values[index] = element;
+                this.Count++;
+            }
+            else
+            {
+                throw new IndexOutOfRangeException("Index was outside the bounds of the list.");
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index >= 0 && index < this.Count)
+            {
+                for (int i = index + 1; i < this.Count; i++)
+                {
+                    this.values[i - 1] = this.values[i];
+                }
+
+                this.values[this.Count--] = default(T);
+            }
+            else
+            {
+                throw new IndexOutOfRangeException("Index was outside the bounds of the list.");
+            }
+        }
+
+        // Generic type comparison workaround: http://stackoverflow.com/questions/6480577/c-sharp-generics-how-to-compare-values-of-generic-types
+        public T Max()
+        {
+
+            if (this.Count <= 0)
+            {
+                throw new InvalidOperationException("The maximum of an empty list cannot be determined.");
+            }
+            else
+            {
+                T max = this.values[0];
+                for (int i = 1; i < this.Count; i++)
+                {
+                    if (max.CompareTo(this.values[i]) < 0)
+                    {
+                        max = this.values[i];
+                    }
+                }
+
+                return max;
+            }
+        }
+
+        public T Min()
+        {
+
+            if (this.Count <= 0)
+            {
+                throw new InvalidOperationException("The minimum of an empty list cannot be determined.");
+            }
+            else
+            {
+                T min = this.values[0];
+                for (int i = 1; i < this.Count; i++)
+                {
+                    if (min.CompareTo(this.values[i]) > 0)
+                    {
+                        min = this.values[i];
+                    }
+                }
+
+                return min;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < this.Count; i++)
+            {
+                sb.Append(string.Format("{0} ", this.values[i]));
+            }
+
+            return sb.ToString();
+        }
+
+        public void Clear()
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                this.values[i] = default(T);
+            }
+
+            this.Count = 0;
         }
 
         private void Resize()
@@ -50,7 +184,7 @@
             int newSize = this.values.Length * 2;
             T[] resizedValues = new T[newSize];
 
-            // Array.Copy would also do..
+            // Array.Copy would also work fine here
             for (int i = 0; i < this.Count; i++)
             {
                 resizedValues[i] = this.values[i];
@@ -60,16 +194,5 @@
             this.Capacity *= 2;
         }
 
-        public void Add(T element)
-        {
-            if (this.Count >= values.Length)
-            {
-                this.Resize();
-            }
-            this.values[this.Count] = element;
-            this.Count++;
-        }
-
-        
     }
 }
--- OOP/2. Defining classes - Part 2/DefiningClassesPartTwo/GenericList/Test.cs
+++ OOP/2. Defining classes - Part 2/DefiningClassesPartTwo/GenericList/Test.cs
@@ -1,16 +1,32 @@
 ﻿namespace GenericList
 {
+    using System;
+
     class Test
     {
         static void Main()
         {
-            GenericList<int> list = new GenericList<int>();
-            // It should resize two times
-            for (int i = 0; i < 17; i++)
+            var someList = new GenericList<int>();
+            // Notice the capacity value here (the list resizes twice)
+            for (int i = 1; i < 19; i++)
             {
-                System.Console.WriteLine("Capacity: {0} Count: {1}", list.Capacity, list.Count);
-                list.Add(i);
+               Console.WriteLine("Current value: {0} Current capacity: {1} Current count: {2}", i, someList.Capacity, someList.Count);
+               someList.Add(i);
             }
+
+            Console.WriteLine(new string('-', Console.BufferWidth));
+            Console.WriteLine("Result: {0}", someList.ToString());
+            Console.WriteLine("List[12]: {0}", someList[12]);
+            Console.WriteLine("Max(): {0}", someList.Max());
+            Console.WriteLine("Min(): {0}", someList.Min());
+            Console.WriteLine("IndexOf(12): {0}", someList.IndexOf(12));
+            Console.WriteLine("IndexOf(1337): {0}", someList.IndexOf(1337));
+            someList.InsertAt(2, 666);
+            Console.WriteLine("InsertAt(2, 666) => {0}", someList.ToString());
+            someList.RemoveAt(2);
+            Console.WriteLine("RemoveAt(2) => {0}", someList.ToString());
+            someList.Clear();
+            Console.WriteLine("Clear() => {0}", someList.ToString());
         }
     }
 }
