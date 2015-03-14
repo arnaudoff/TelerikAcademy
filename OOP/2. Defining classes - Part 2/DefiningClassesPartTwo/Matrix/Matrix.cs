5babd96c116cef5ff898425d91229bb897452855
r1f <ivaylo.arnaudov12@gmail.com>
2015-03-14 20:00:26 +0200

    Added matrices

---
+++ OOP/2. Defining classes - Part 2/DefiningClassesPartTwo/Matrix/Matrix.cs
@@ -0,0 +1,164 @@
+﻿﻿namespace Matrices
+{
+    using System;
+    using System.Text;
+
+    public class Matrix<T> where T : IComparable
+    {
+        private const string delimiter = " ";
+        private T[,] matrix;
+
+        public Matrix(int rows, int cols)
+        {
+            this.Rows = rows;
+            this.Cols = cols;
+            this.matrix = new T[rows, cols];
+        }
+
+        public int Rows { get; private set; }
+
+        public int Cols { get; private set; }
+        
+        public T this[int row, int col]
+        {
+            get
+            {
+                if ((row < 0 || row >= this.Rows) || (col < 0 || col >= this.Cols))
+                {
+                    throw new IndexOutOfRangeException("Invalid index given.");
+                }
+
+                return this.matrix[row, col];
+            }
+            set
+            {
+                if ((row < 0 || row >= this.Rows) || (col < 0 || col >= this.Cols))
+                {
+                    throw new IndexOutOfRangeException("Invalid index given.");
+                }
+
+                this.matrix[row, col] = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder res = new StringBuilder();
+
+            for (int row = 0; row < this.Rows; row++)
+            {
+                for (int col = 0; col < this.Cols; col++)
+                {
+                    res.Append(this.matrix[row, col] + delimiter);
+                }
+                res.AppendLine();
+            }
+
+            return res.ToString();
+        }
+
+        public static Matrix<T> operator +(Matrix<T> firstMatrix, Matrix<T> secondMatrix)
+        {
+            if (firstMatrix.Cols != secondMatrix.Cols || firstMatrix.Rows != secondMatrix.Rows)
+            {
+                throw new InvalidOperationException("Matrices are with different dimensions and cannot be added.");
+            }
+
+            Matrix<T> res = new Matrix<T>(firstMatrix.Rows, firstMatrix.Cols);
+
+            for (int row = 0; row < firstMatrix.Rows; row++)
+            {
+                for (int col = 0; col < firstMatrix.Cols; col++)
+                {
+                    res[row, col] = (dynamic)firstMatrix[row, col] + secondMatrix[row, col];
+                }
+            }
+
+            return res;
+        }
+
+        public static Matrix<T> operator -(Matrix<T> firstMatrix, Matrix<T> secondMatrix)
+        {
+            if (firstMatrix.Cols != secondMatrix.Cols || firstMatrix.Rows != secondMatrix.Rows)
+            {
+                throw new InvalidOperationException("Matrices are with different dimensions and cannot be substracted.");
+            }
+
+            Matrix<T> res = new Matrix<T>(firstMatrix.Rows, firstMatrix.Cols);
+
+            for (int row = 0; row < firstMatrix.Rows; row++)
+            {
+                for (int col = 0; col < firstMatrix.Cols; col++)
+                {
+                    res[row, col] = (dynamic)firstMatrix[row, col] - secondMatrix[row, col];
+                }
+            }
+
+            return res;
+        }
+
+        public static Matrix<T> operator *(Matrix<T> firstMatrix, Matrix<T> secondMatrix)
+        {
+            if (firstMatrix.Cols != secondMatrix.Rows)
+            {
+                throw new InvalidOperationException("Matrices are with different dimensions and cannot be multiplied.");
+            }
+
+            Matrix<T> res = new Matrix<T>(firstMatrix.Rows, secondMatrix.Cols);
+            T tmp;
+
+            for (int matrixRow = 0; matrixRow < res.Rows; matrixRow++)
+            {
+                for (int matrixCol = 0; matrixCol < res.Cols; matrixCol++)
+                {
+                    tmp = default(T);
+                    for (int index = 0; index < res.Cols; index++)
+                    {
+                        tmp += (dynamic)firstMatrix[matrixRow, index] * secondMatrix[index, matrixCol];
+                    }
+                    res[matrixRow, matrixCol] = tmp;
+                }
+            }
+
+            return res;
+        }
+
+        public void Fill()
+        {
+            for (int row = 0; row < this.Rows; row++)
+            {
+                string[] currentRow = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                for (int col = 0; col < this.Cols; col++)
+                {
+                    this.matrix[row, col] = (dynamic)currentRow[col];
+                }
+            }
+        }
+
+        public static bool operator true(Matrix<T> matrix)
+        {
+            return HasNonZeroValue(matrix);
+        }
+
+        public static bool operator false(Matrix<T> matrix)
+        {
+            return HasNonZeroValue(matrix);
+        }
+
+        private static bool HasNonZeroValue(Matrix<T> matrix)
+        {
+            for (int row = 0; row < matrix.Rows; row++)
+            {
+                for (int col = 0; col < matrix.Cols; col++)
+                {
+                    if (matrix[row, col] != (dynamic)0)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+ }
---
+++ OOP/2. Defining classes - Part 2/DefiningClassesPartTwo/Matrix/Test.cs
@@ -0,0 +1,39 @@
+﻿namespace Matrices
+{
+    using System;
+
+    class Test
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter first matrix row count: ");
+            int firstRow = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter first matrix column count: ");
+            int firstCol = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second matrix row count: ");
+            int secondRow = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second matrix column count: ");
+            int secondCol = int.Parse(Console.ReadLine());
+
+            Matrix<int> first = new Matrix<int>(firstRow, firstCol);
+            Matrix<int> second = new Matrix<int>(secondRow, secondCol);
+
+            first.Fill();
+            second.Fill();
+            Matrix<int> res = first * second;
+
+            Console.WriteLine("Multiplication result: ");
+            for (int row = 0; row < res.Rows; row++)
+            {
+                for (int col = 0; col < res.Cols; col++)
+                {
+                    Console.Write("{0} ", res[row, col]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("As a string: ");
+            Console.WriteLine(res.ToString());
+        }
+    }
+}
