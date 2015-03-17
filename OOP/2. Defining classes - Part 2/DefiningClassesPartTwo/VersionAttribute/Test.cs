d0354eb670c181d78f8104e9a07354d065c8d0c7
r1f <ivaylo.arnaudov12@gmail.com>
2015-03-17 22:08:15 +0200

    Finished defining classes part two

---
+++ OOP/2. Defining classes - Part 2/DefiningClassesPartTwo/VersionAttribute/Test.cs
@@ -0,0 +1,19 @@
+﻿namespace VersionAttribute
+{
+    using System;
+    using System.Reflection;
+
+    [VersionAttribute("6.66")]
+    public class Test
+    {
+        static void Main()
+        {
+            var classAttributesList = typeof(Test).GetCustomAttributes<VersionAttribute>();
+
+            foreach (var attribute in classAttributesList)
+            {
+                Console.WriteLine("Version: {0}", attribute.Version);
+            }
+        }
+    }
+}
---
+++ OOP/2. Defining classes - Part 2/DefiningClassesPartTwo/VersionAttribute/VersionAttribute.cs
@@ -0,0 +1,17 @@
+﻿namespace VersionAttribute
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | 
+                    AttributeTargets.Interface | AttributeTargets.Enum | 
+                    AttributeTargets.Method)]
+    public class VersionAttribute : Attribute
+    {
+        public VersionAttribute(string version)
+        {
+            this.Version = version;
+        }
+
+        public string Version { get; private set; }
+    }
+}
