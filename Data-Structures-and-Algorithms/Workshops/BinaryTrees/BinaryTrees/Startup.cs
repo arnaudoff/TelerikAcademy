0fff5559e35653b9808e813906088da00a15a798
Ivaylo Arnaudov <ivaylo.arnaudov12@gmail.com>
2015-11-15 12:51:40 +0200

    Added solutions for BinaryTrees and Precision from the workshop

---
+++ Data-Structures-and-Algorithms/Workshops/BinaryTrees/BinaryTrees/Startup.cs
@@ -0,0 +1,70 @@
+﻿namespace BinaryTrees
+{
+    using System;
+    using System.Numerics;
+
+    public class Startup
+    {
+        private static BigInteger[] memo;
+
+        private static BigInteger CountTrees(int numkeys)
+        {
+            if (memo[numkeys] > 0)
+            {
+                return memo[numkeys];
+            }
+
+            if (numkeys > 1)
+            {
+                int i = 1;
+                BigInteger sum = 0;
+
+                for (i = 1; i <= numkeys; i++)
+                {
+
+                    BigInteger lcount = CountTrees(i - 1);
+                    BigInteger rcount = CountTrees(numkeys - i);
+                    sum += lcount * rcount;
+                }
+
+                memo[numkeys] = sum;
+                return (sum);
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+            int n = input.Length;
+
+            int[] groups = new int[26];
+
+            for (int i = 0; i < n; i++)
+            {
+                groups[input[i] - 'A']++;
+            }
+
+            long[] factorials = new long[n + 1];
+            factorials[0] = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                factorials[i + 1] = factorials[i] * (i + 1);
+            }
+
+            BigInteger result = factorials[n];
+
+            for (int i = 0; i < groups.Length; i++)
+            {
+                result = result / factorials[groups[i]];
+            }
+
+            memo = new BigInteger[n + 1];
+            Console.WriteLine(result * CountTrees(n));
+        }
+    }
+}
---
+++ Data-Structures-and-Algorithms/Workshops/Precision/Precision/Startup.cs
@@ -0,0 +1,107 @@
+﻿namespace Precision
+{
+    using System;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            int maxDenominator = int.Parse(Console.ReadLine());
+            string number = Console.ReadLine();
+
+            int bestNominator = 0;
+            int bestDenominator = 1;
+            int bestPrecision = 0;
+
+            for (int denominator = 1; denominator <= maxDenominator; denominator++)
+            {
+                int left = 0;
+                int right = denominator;
+
+                while (left < right)
+                {
+                    int middle = (left + right) / 2;
+                    if (Compare(middle, denominator, number))
+                    {
+                        right = middle;
+                    }
+                    else
+                    {
+                        left = middle + 1;
+                    }
+                }
+
+                int currentPrecision = GetPrecision(left, denominator, number);
+                if (currentPrecision > bestPrecision)
+                {
+                    bestDenominator = denominator;
+                    bestNominator = left;
+                    bestPrecision = currentPrecision;
+                }
+
+                currentPrecision = GetPrecision(left - 1, denominator, number);
+                if (currentPrecision > bestPrecision)
+                {
+                    bestDenominator = denominator;
+                    bestNominator = left - 1;
+                    bestPrecision = currentPrecision;
+                }
+            }
+
+            Console.WriteLine("{0}/{1}", bestNominator, bestDenominator);
+            Console.WriteLine(bestPrecision);
+        }
+
+        private static bool Compare(int a, int b, string number)
+        {
+            int i = 0;
+            while (i < number.Length)
+            {
+                if (number[i] == '.')
+                {
+                    i++;
+                    continue;
+                }
+
+                int currentDigit = a / b;
+                if (currentDigit < number[i] - '0')
+                {
+                    return false;
+                }
+                else if (currentDigit > number[i] - '0') 
+                {
+                    return true;
+                }
+
+                a = a % b * 10;
+                i++;
+            }
+
+            return true;
+        }
+
+        private static int GetPrecision(int a, int b, string number)
+        {
+            int i = 0;
+            while (i < number.Length)
+            {
+                if (number[i] == '.')
+                {
+                    i++;
+                    continue;
+                }
+
+                int currentDigit = a / b;
+                if (currentDigit != number[i] - '0')
+                {
+                    break;
+                }
+
+                a = a % b * 10;
+                i++;
+            }
+
+            return i - 1;
+        }
+    }
+}
